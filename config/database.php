ede77cb61349da235ebe13ed73407a6c52e8b8be
egor_wglzfg <actzch@gmail.com>
2023-09-07 07:27:49 +0300

    chore: add migrations

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'dbname' => env('DB_NAME'),
+    'host' => env('DB_HOST'),
+    'usermane' => env('DB_USER'),
+    'password' => env('DB_PASSWORD'),
+];
---
+++ phpmig.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__.'/bootstrap.php';
+
+$container = new ArrayObject();
+$db_config = require __DIR__.'/config/database.php';
+
+$container['db'] = function () use ($db_config) {
+    $dbh = new PDO(sprintf('mysql:dbname=%s;host=%s', $db_config['dbname'], $db_config['host']), $db_config['usermane'], $db_config['password']);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbh;
+};
+
+$container['phpmig.adapter'] = function ($c) {
+    return new \Phpmig\Adapter\PDO\Sql($c['db'], 'migrations');
+};
+
+$container['phpmig.migrations_path'] = __DIR__ . DIRECTORY_SEPARATOR . 'database'. DIRECTORY_SEPARATOR . 'migrations';
+
+return $container;

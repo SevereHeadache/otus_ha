206d64c1bd54d99ddc4e4821d42727ca8394a906
SevereHeadache <oysterpost@proton.me>
2023-09-17 16:49:01 +0300

    fix: docker build

--- src/Repositories/Database/DB.php
+++ src/Repositories/Database/DB.php
@@ -13,10 +13,9 @@ class DB
         if (!isset($connection)){
             $db_config = require PROJECT_PATH.'/config/database.php';
             $connection = new PDO(sprintf(
-                'pgsql:dbname=%s;host=%s;port=%s;password=%s;',
+                'pgsql:dbname=%s;host=%s;password=%s;',
                 $db_config['dbname'],
                 $db_config['host'],
-                $db_config['port'],
                 $db_config['password']
             ), $db_config['usermane'], $db_config['password']);
             $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
